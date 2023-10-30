e04924737969708f13eb0230429729137537466c
ajuteixeira <teixeirajuuh@gmail.com>
2023-10-30 11:46:48 -0300

    criação de tb_aluno

--- aula06/backend/db.sql
+++ aula06/backend/db.sql
@@ -16,3 +16,22 @@ VALUES ('Banco de Dados', '16');
 
 INSERT INTO tb_disciplina (nome, carga_horaria)
 VALUES ('NodeJS', '12');
+
+---
+
+CREATE TABLE tb_aluno (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR (30) NOT NULL,
+    cpf CHAR (11) NOT NULL,
+    email VARCHAR (30) NOT NULL,
+    turma VARCHAR (4) NOT NULL
+);
+
+INSERT INTO tb_aluno (nome, cpf, email, turma)
+VALUES('Juliana', '12345678911', 'juliana@email.com', 'fs17');
+
+INSERT INTO tb_aluno (nome, cpf, email, turma)
+VALUES ('Maria', '98765432101', 'maria@email.com', 'fs15');
+
+INSERT INTO tb_aluno (nome, cpf, email, turma)
+VALUES ('Gustavo', '11111111111', 'gustavo@email.com', 'fs19');
